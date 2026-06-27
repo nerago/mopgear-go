32534adf17c1c2fdea437691c5a1938d8cb5abec
Nicholas Hardy <nicholas.h.hardy@gmail.com>
2026-06-28 04:08:40 +1000

    future tests

--- nerago-gearing/util/channel_op/Future.go
+++ nerago-gearing/util/channel_op/Future.go
@@ -1,6 +1,7 @@
 package channel_op
 
 import (
+	"fmt"
 	"os"
 	"sync"
 )
@@ -58,114 +59,146 @@ func (future *Future[T]) WaitForResultOrPanic() T {
 
 // ########### FutureCancellable ###########
 type FutureCancellable[T any] struct {
-	signalChannel chan any
-	result        T
-	isValidResult bool
+	signalChannel chan futureResult[T]
 	isComplete    bool
+	isCancelled   bool
+	hasWaiter     bool
 	onCancel      []func()
 	lock          sync.Mutex
 }
 
+type futureResult[T any] struct {
+	value    T
+	hasValue bool
+}
+
 func FutureCancellable_Make[T any]() *FutureCancellable[T] {
+	sc := make(chan futureResult[T], 1)
 	return &FutureCancellable[T]{
-		signalChannel: make(chan any, 1),
+		signalChannel: sc,
 	}
 }
 
 func (future *FutureCancellable[T]) SetResult(value T) {
 	future.lock.Lock()
-	defer future.lock.Unlock()
-
 	if !future.isComplete {
-		future.result = value
-		future.isValidResult = true
 		future.isComplete = true
 		future.onCancel = nil
-		future.signalChannel <- true
+		future.lock.Unlock()
+		future.signalChannel <- futureResult[T]{value: value, hasValue: true}
+	} else {
+		future.lock.Unlock()
 	}
 }
 
 func (future *FutureCancellable[T]) SetResultEmpty() {
 	future.lock.Lock()
-	defer future.lock.Unlock()
-
 	if !future.isComplete {
-		future.isValidResult = false
 		future.isComplete = true
 		future.onCancel = nil
-		future.signalChannel <- true
+		future.lock.Unlock()
+		future.signalChannel <- futureResult[T]{hasValue: false}
+	} else {
+		future.lock.Unlock()
 	}
 }
 
 func (future *FutureCancellable[T]) AddCancelHandler(onCancel func()) {
 	future.lock.Lock()
-	defer future.lock.Unlock()
 
-	if future.IsCancelled() {
+	if future.isCancelled {
+		future.lock.Unlock()
 		onCancel()
 	} else {
 		future.onCancel = append(future.onCancel, onCancel)
+		future.lock.Unlock()
 	}
 }
 
 func (future *FutureCancellable[T]) Cancel() {
 	future.lock.Lock()
-	defer future.lock.Unlock()
 
 	if !future.isComplete {
-		future.isValidResult = false
 		future.isComplete = true
+		future.isCancelled = true
 		for i := range future.onCancel {
 			future.onCancel[i]()
 		}
 		future.onCancel = nil
-		future.signalChannel <- true
+		future.lock.Unlock()
+		future.signalChannel <- futureResult[T]{hasValue: false}
+	} else {
+		future.lock.Unlock()
 	}
 }
 
+// IsCancelled also includes normal completion, intended for use in loop conditions etc
 func (future *FutureCancellable[T]) IsCancelled() bool {
-	return future.isComplete
+	return future.isComplete || future.isCancelled
 }
 
 func (future *FutureCancellable[T]) WaitForResult() (T, bool) {
-	<-future.signalChannel
-	return future.result, future.isValidResult
-}
-
-func (future *FutureCancellable[T]) WaitForResultOrKeyPress() (T, bool) {
-	channelForKey := future.channelKeyPress()
-
-	select {
-	case <-future.signalChannel:
-		return future.result, future.isValidResult
-
-	case <-channelForKey:
-		future.lock.Lock()
-		defer future.lock.Unlock()
-
-		// if we're here then noone is listening for signal anymore, but someone could still have beaten us to lock
-		if future.isComplete {
-			return future.result, future.isValidResult
-		} else {
-			future.isValidResult = false
-			future.isComplete = true
-			for i := range future.onCancel {
-				future.onCancel[i]()
-			}
-			future.onCancel = nil
-			return future.result, false
-		}
+	if future == nil || future.signalChannel == nil {
+		panic("invalid future")
+	} else if future.hasWaiter {
+		panic("duplicate waiter")
 	}
+	future.hasWaiter = true
+
+	os.Stdout.WriteString("WaitForResult before\n")
+	result, channelOk := <-future.signalChannel
+	if !channelOk {
+		panic("signal channel closed")
+	}
+	close(future.signalChannel)
+
+	fmt.Printf("WaitForResult after %v %v\n", result.value, result.hasValue)
+	return result.value, result.hasValue
 }
 
-func (*FutureCancellable[T]) channelKeyPress() chan any {
-	channelForKey := make(chan any)
-	go func() {
-		_, err := os.Stdin.Read([]byte{0})
-		if err != nil {
-			panic(err)
-		}
-		channelForKey <- true
-	}()
-	return channelForKey
-}
+//func (future *FutureCancellable[T]) WaitForResultOrKeyPress() (T, bool) {
+//	if future == nil || future.signalChannel == nil {
+//		panic("bad future")
+//	}
+//
+//	channelForKey := future.channelKeyPress()
+//
+//	select {
+//	case <-future.signalChannel:
+//		return future.result, future.isValidResult
+//
+//	case <-channelForKey:
+//		future.lock.Lock()
+//		defer future.lock.Unlock()
+//
+//		// if we're here then noone is listening for signal anymore, but someone could still have beaten us to lock
+//		if future.isComplete {
+//			return future.result, future.isValidResult
+//		} else {
+//			_, err := os.Stdout.WriteString("Cancelling on key press\n")
+//			if err != nil {
+//				panic(err)
+//			}
+//
+//			future.isValidResult = false
+//			future.isComplete = true
+//			for i := range future.onCancel {
+//				future.onCancel[i]()
+//			}
+//			future.onCancel = nil
+//			return future.result, false
+//		}
+//	}
+//}
+
+//func (*FutureCancellable[T]) channelKeyPress() chan any {
+//	channelForKey := make(chan any)
+//	go func() {
+//		_, err := os.Stdin.Read([]byte{0})
+//		if err != nil {
+//			panic(err)
+//		}
+//		channelForKey <- true
+//	}()
+//	return channelForKey
+//}
---
+++ nerago-gearing/util/channel_op/Future_test.go
@@ -0,0 +1,313 @@
+package channel_op
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestFlowSetResult(t *testing.T) {
+	waitGroup := sync.WaitGroup{}
+	future := FutureCancellable_Make[int]()
+	calledCancelHandler := false
+	future.AddCancelHandler(func() {
+		calledCancelHandler = true
+	})
+	waitGroup.Go(func() {
+		start := time.Now()
+		value, ok := future.WaitForResult()
+		if !ok {
+			t.Error("bad ok")
+		}
+		if value != 123 {
+			t.Error("bad value")
+		}
+		taken := time.Since(start)
+		if taken < 2*time.Second {
+			t.Error("finished too fast")
+		}
+	})
+	waitGroup.Go(func() {
+		time.Sleep(2 * time.Second)
+		future.SetResult(123)
+	})
+	waitGroup.Wait()
+	if calledCancelHandler {
+		t.Error("called cancel handler")
+	}
+}
+
+func TestFlowSetResultLateWaiter(t *testing.T) {
+	waitGroup := sync.WaitGroup{}
+	future := FutureCancellable_Make[int]()
+	calledCancelHandler := false
+	future.AddCancelHandler(func() {
+		calledCancelHandler = true
+	})
+	waitGroup.Go(func() {
+		time.Sleep(2 * time.Second)
+		value, ok := future.WaitForResult()
+		if !ok {
+			t.Error("bad ok")
+		}
+		if value != 123 {
+			t.Error("bad value")
+		}
+	})
+	waitGroup.Go(func() {
+		time.Sleep(1 * time.Second)
+		future.SetResult(123)
+	})
+	waitGroup.Wait()
+	if calledCancelHandler {
+		t.Error("called cancel handler")
+	}
+}
+
+func TestFlowSetEmpty(t *testing.T) {
+	waitGroup := sync.WaitGroup{}
+	future := FutureCancellable_Make[int]()
+	calledCancelHandler := false
+	future.AddCancelHandler(func() {
+		calledCancelHandler = true
+	})
+	waitGroup.Go(func() {
+		start := time.Now()
+		value, ok := future.WaitForResult()
+		if ok {
+			t.Error("bad ok")
+		}
+		if value != 0 {
+			t.Error("bad value")
+		}
+		taken := time.Since(start)
+		if taken < 2*time.Second {
+			t.Error("finished too fast")
+		}
+	})
+	waitGroup.Go(func() {
+		time.Sleep(2 * time.Second)
+		future.SetResultEmpty()
+	})
+	waitGroup.Wait()
+	if calledCancelHandler {
+		t.Error("called cancel handler")
+	}
+}
+
+func TestFlowSetEmptyLateWaiter(t *testing.T) {
+	waitGroup := sync.WaitGroup{}
+	future := FutureCancellable_Make[int]()
+	calledCancelHandler := false
+	future.AddCancelHandler(func() {
+		calledCancelHandler = true
+	})
+	waitGroup.Go(func() {
+		time.Sleep(2 * time.Second)
+		value, ok := future.WaitForResult()
+		if ok {
+			t.Error("bad ok")
+		}
+		if value != 0 {
+			t.Error("bad value")
+		}
+	})
+	waitGroup.Go(func() {
+		time.Sleep(1 * time.Second)
+		future.SetResultEmpty()
+	})
+	waitGroup.Wait()
+	if calledCancelHandler {
+		t.Error("called cancel handler")
+	}
+}
+
+func TestFlowCancel(t *testing.T) {
+	waitGroup := sync.WaitGroup{}
+	future := FutureCancellable_Make[int]()
+	calledCancelHandler := false
+	future.AddCancelHandler(func() {
+		calledCancelHandler = true
+	})
+	waitGroup.Go(func() {
+		start := time.Now()
+		value, ok := future.WaitForResult()
+		if ok {
+			t.Error("bad ok")
+		}
+		if value != 0 {
+			t.Error("bad value")
+		}
+		taken := time.Since(start)
+		if taken < 2*time.Second {
+			t.Error("finished too fast")
+		}
+	})
+	waitGroup.Go(func() {
+		time.Sleep(2 * time.Second)
+		future.Cancel()
+	})
+	waitGroup.Wait()
+	if !calledCancelHandler {
+		t.Error("didn't call cancel handler")
+	}
+}
+
+func TestFlowCancelLateWaiter(t *testing.T) {
+	waitGroup := sync.WaitGroup{}
+	future := FutureCancellable_Make[int]()
+	calledCancelHandler := false
+	future.AddCancelHandler(func() {
+		calledCancelHandler = true
+	})
+	waitGroup.Go(func() {
+		time.Sleep(2 * time.Second)
+		value, ok := future.WaitForResult()
+		if ok {
+			t.Error("bad ok")
+		}
+		if value != 0 {
+			t.Error("bad value")
+		}
+	})
+	waitGroup.Go(func() {
+		time.Sleep(1 * time.Second)
+		future.Cancel()
+	})
+	waitGroup.Wait()
+	if !calledCancelHandler {
+		t.Error("didn't call cancel handler")
+	}
+}
+
+func TestFlowCancelThenDone(t *testing.T) {
+	waitGroup := sync.WaitGroup{}
+	future := FutureCancellable_Make[int]()
+	calledCancelHandler := false
+	future.AddCancelHandler(func() {
+		calledCancelHandler = true
+	})
+	waitGroup.Go(func() {
+		start := time.Now()
+		value, ok := future.WaitForResult()
+		if ok {
+			t.Error("bad ok")
+		}
+		if value != 0 {
+			t.Error("bad value")
+		}
+		taken := time.Since(start)
+		if taken < 1*time.Second {
+			t.Error("finished too fast")
+		}
+	})
+	waitGroup.Go(func() {
+		time.Sleep(2 * time.Second)
+		future.SetResult(123)
+	})
+	waitGroup.Go(func() {
+		time.Sleep(1 * time.Second)
+		future.Cancel()
+	})
+	waitGroup.Wait()
+	if !calledCancelHandler {
+		t.Error("didn't call cancel handler")
+	}
+}
+
+func TestFlowCancelThenDoneLateWaiter(t *testing.T) {
+	waitGroup := sync.WaitGroup{}
+	future := FutureCancellable_Make[int]()
+	calledCancelHandler := false
+	future.AddCancelHandler(func() {
+		calledCancelHandler = true
+	})
+	waitGroup.Go(func() {
+		time.Sleep(3 * time.Second)
+		value, ok := future.WaitForResult()
+		if ok {
+			t.Error("bad ok")
+		}
+		if value != 0 {
+			t.Error("bad value")
+		}
+	})
+	waitGroup.Go(func() {
+		time.Sleep(2 * time.Second)
+		future.SetResult(123)
+	})
+	waitGroup.Go(func() {
+		time.Sleep(1 * time.Second)
+		future.Cancel()
+	})
+	waitGroup.Wait()
+	if !calledCancelHandler {
+		t.Error("didn't call cancel handler")
+	}
+}
+
+func TestFlowDoneThenCancel(t *testing.T) {
+	waitGroup := sync.WaitGroup{}
+	future := FutureCancellable_Make[int]()
+	calledCancelHandler := false
+	future.AddCancelHandler(func() {
+		calledCancelHandler = true
+	})
+	waitGroup.Go(func() {
+		start := time.Now()
+		value, ok := future.WaitForResult()
+		if !ok {
+			t.Error("bad ok")
+		}
+		if value != 123 {
+			t.Error("bad value")
+		}
+		taken := time.Since(start)
+		if taken < 1*time.Second {
+			t.Error("finished too fast")
+		}
+	})
+	waitGroup.Go(func() {
+		time.Sleep(1 * time.Second)
+		future.SetResult(123)
+	})
+	waitGroup.Go(func() {
+		time.Sleep(2 * time.Second)
+		future.Cancel()
+	})
+	waitGroup.Wait()
+	if calledCancelHandler {
+		t.Error("called cancel handler")
+	}
+}
+
+func TestFlowDoneThenCancelLaterWaiter(t *testing.T) {
+	waitGroup := sync.WaitGroup{}
+	future := FutureCancellable_Make[int]()
+	calledCancelHandler := false
+	future.AddCancelHandler(func() {
+		calledCancelHandler = true
+	})
+	waitGroup.Go(func() {
+		time.Sleep(3 * time.Second)
+		value, ok := future.WaitForResult()
+		if !ok {
+			t.Error("bad ok")
+		}
+		if value != 123 {
+			t.Error("bad value")
+		}
+	})
+	waitGroup.Go(func() {
+		time.Sleep(1 * time.Second)
+		future.SetResult(123)
+	})
+	waitGroup.Go(func() {
+		time.Sleep(2 * time.Second)
+		future.Cancel()
+	})
+	waitGroup.Wait()
+	if calledCancelHandler {
+		t.Error("called cancel handler")
+	}
+}
